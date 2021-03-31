3440978781382d3832d816a3386b721a8e9eaa89
Kevin Ian Ruiz Vargas <kvargas@motorola.com>
2021-03-31 17:13:50 -0300

    adding FrozenBN

--- san_detectron/san_backbone.py
+++ san_detectron/san_backbone.py
@@ -160,94 +160,38 @@ class SAN(Backbone):
     def __init__(self, cfg, sa_type, block, layers, kernels, num_classes):
         super(SAN, self).__init__()
 
-        self.stage_names = []
-        #First Block
-        dict1 = OrderedDict()
         c = 64
-        conv_in, bn_in = conv1x1(3, c), nn.BatchNorm2d(c)
-        conv0, bn0 = conv1x1(c, c), nn.BatchNorm2d(c)
-        layer0 = self._make_layer(
+        self.conv_in, self.bn_in = conv1x1(3, c), nn.BatchNorm2d(c)
+        self.conv0, self.bn0 = conv1x1(c, c), nn.BatchNorm2d(c)
+        self.layer0 = self._make_layer(
             sa_type, block, c, layers[0], kernels[0])
 
-        dict1['conv_in'] = conv_in
-        dict1['bn_in'] = bn_in
-        dict1['conv0'] = conv0
-        dict1['bn0'] = bn0
-        dict1['layer0'] = layer0
-
-        self.add_module("stem", nn.Sequential(dict1))
-
-        #Second Block
-        dict2 = OrderedDict()
         c *= 4
-        conv1, bn1 = conv1x1(c // 4, c), nn.BatchNorm2d(c)
-        layer1 = self._make_layer(
+        self.conv1, self.bn1 = conv1x1(c // 4, c), nn.BatchNorm2d(c)
+        self.layer1 = self._make_layer(
             sa_type, block, c, layers[1], kernels[1])
 
-        dict2['conv1'] = conv1
-        dict2['bn1'] = bn1
-        dict2['layer1'] = layer1
-
-        self.stage_names.append("res2")
-        self.add_module("res2", nn.Sequential(dict2))
-
-        #Third Block
-        dict3 = OrderedDict()
         c *= 2
-        conv2, bn2 = conv1x1(c // 2, c), nn.BatchNorm2d(c)
-        layer2 = self._make_layer(
+        self.conv2, self.bn2 = conv1x1(c // 2, c), nn.BatchNorm2d(c)
+        self.layer2 = self._make_layer(
             sa_type, block, c, layers[2], kernels[2])
 
-        dict3['conv2'] = conv2
-        dict3['bn2'] = bn2
-        dict3['layer2'] = layer2
-
-        self.stage_names.append("res3")
-        self.add_module("res3", nn.Sequential(dict3))
-
-        #Fourth Block
-        dict4 = OrderedDict()
         c *= 2
-        conv3, bn3 = conv1x1(c // 2, c), nn.BatchNorm2d(c)
-        layer3 = self._make_layer(
+        self.conv3, self.bn3 = conv1x1(c // 2, c), nn.BatchNorm2d(c)
+        self.layer3 = self._make_layer(
             sa_type, block, c, layers[3], kernels[3])
 
-        dict4['conv3'] = conv3
-        dict4['bn3'] = bn3
-        dict4['layer3'] = layer3
-
-        self.stage_names.append("res4")
-        self.add_module("res4", nn.Sequential(dict4))
-
-        #Fifth Block
-        dict5 = OrderedDict()
         c *= 2
-        conv4, bn4 = conv1x1(c // 2, c), nn.BatchNorm2d(c)
-        layer4 = self._make_layer(
+        self.conv4, self.bn4 = conv1x1(c // 2, c), nn.BatchNorm2d(c)
+        self.layer4 = self._make_layer(
             sa_type, block, c, layers[4], kernels[4])
 
-        # self.relu = nn.ReLU(inplace=True)
-        # self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        # self.fc = nn.Linear(c, num_classes)
+        self.relu = nn.ReLU(inplace=True)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        dict5['conv4'] = conv4
-        dict5['bn4'] = bn4
-        dict5['layer4'] = layer4
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(c, num_classes)
 
-        self.stage_names.append("res5")
-        self.add_module("res5", nn.Sequential(dict5))
-
-
-        # self._initialize_weights()
-        # self._freeze_backbone(cfg.MODEL.BACKBONE.FREEZE_AT)
-
-    def _freeze_backbone(self, freeze_at):
-        print("freeze_at:")
-        print(freeze_at)
-        for layer_index in range(freeze_at):
-            for p in self.features[layer_index].parameters():
-                p.requires_grad = False
 
     def _make_layer(self, sa_type, block, planes, blocks, kernel_size=7, stride=1):
         layers = []
@@ -257,56 +201,61 @@ class SAN(Backbone):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        # x = x[:, [2, 1, 0], : , :]
         outputs = {}
-        x = self.stem(x)
-        # if "stem" in self._out_features:
-        outputs["stem"] = x
-        for name in self.stage_names:
-            print("name")
-            print(name)
-            x = getattr(self, name)(x)
-            # if name in self._out_features:
-            outputs[name] = x
+        # x = x[:, [2, 1, 0], : , :]
+        x = self.relu(self.bn_in(self.conv_in(x)))
+        x = self.relu(self.bn0(self.layer0(self.conv0(self.pool(x))))) #64
+        x = self.relu(self.bn1(self.layer1(self.conv1(self.pool(x))))) #256
+        outputs['res2'] = x
+        x = self.relu(self.bn2(self.layer2(self.conv2(self.pool(x))))) #512
+        outputs['res3'] = x
+        x = self.relu(self.bn3(self.layer3(self.conv3(self.pool(x))))) #1024
+        outputs['res4'] = x
+        x = self.relu(self.bn4(self.layer4(self.conv4(self.pool(x))))) #2048
+        outputs['res5'] = x
 
+        # for key in outputs:
+        #     print ("key: ", key)
+        #     print ("value: ", outputs[key].shape)
+
+        # x = self.avgpool(x)
+        # x = x.view(x.size(0), -1)
+        # x = self.fc(x)
         return outputs
 
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, (2. / n) ** 0.5)
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                n = m.weight.size(1)
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
-
 def san(cfg, sa_type, block, layers, kernels, num_classes):
-    model = SAN(cfg, sa_type, block, layers, kernels, num_classes)
-    # model = torch.nn.DataParallel(model.cuda())
-    # checkpoint = torch.load("../SAN/exp/imagenet/san19_patchwise/model/model_best.pth", map_location= "cpu")
-    # model.load_state_dict(checkpoint['state_dict'], strict=True)
-    # toModules = [module for module in model.modules() if not isinstance(module, nn.Sequential)]
-    # return toModules[1]
-    return model
+    sanModel = SAN(cfg, sa_type, block, layers, kernels, num_classes)
+    sanModel = torch.nn.DataParallel(sanModel.cuda())
+    checkpoint = torch.load("../SAN/exp/imagenet/san19_patchwise/model/model_best.pth", map_location= "cpu")
+    sanModel.load_state_dict(checkpoint['state_dict'], strict=True)
+
+    #Freeze first 2 layers
+    frozen_range = [
+                  sanModel.module.conv_in,
+                  sanModel.module.bn_in,
+                  sanModel.module.conv0,
+                  sanModel.module.bn0,
+                  sanModel.module.layer0
+                ]
+
+    for module in frozen_range:
+        for param in module.parameters():
+            param.requires_grad = False
+
+    sanModel.module = FrozenBatchNorm2d.convert_frozen_batchnorm(sanModel.module)
+    toModules = [module for module in sanModel.modules() if not isinstance(module, nn.Sequential)]
+    return toModules[1]
+
 
 @BACKBONE_REGISTRY.register()
 def build_san_backbone(cfg, input_shape):
-
     """
     Create a SAN instance from config.
 
     Returns:
         SAN: a :class:`SAN` instance.
     """
-
-    out_features = cfg.MODEL.SAN.OUT_FEATURES
-
+    out_features = ["res2", "res3", "res4", "res5"]
     out_feature_channels = {"res2": 256, "res3": 512,
                             "res4": 1024, "res5": 2048}
     out_feature_strides = {"res2": 4, "res3": 8, "res4": 16, "res5": 32}
@@ -328,11 +277,11 @@ def build_san_backbone(cfg, input_shape):
                 num_classes=san_num_classes)
 
 
+
     model._out_features = out_features
     model._out_feature_channels = out_feature_channels
     model._out_feature_strides = out_feature_strides
     return model
-
 
 
 @BACKBONE_REGISTRY.register()
